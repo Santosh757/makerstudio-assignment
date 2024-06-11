299f641c1fe0ac84d73d3fac0f0aa14f2d5eadde
santosh-sahu-7 <s.santoshsah0027@gmail.com>
2024-06-11 22:25:53 +0530

    Add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
   import "../app.css";
   import type { LayoutData } from "./$types";
   import { products } from "$lib/stores/product";
+  import Navbar from "$lib/components/Navbar.svelte";
 
   export let data: LayoutData;
 
@@ -12,4 +13,7 @@
   });
 </script>
 
-<slot />
+<div class="max-w-7xl mx-auto min-h-screen px-5">
+  <Navbar />
+  <slot />
+</div>
