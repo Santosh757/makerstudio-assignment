270ee4bcf230b5469bbe14e0ad81f0f1c070e9b0
santosh-sahu-7 <s.santoshsah0027@gmail.com>
2024-06-11 22:02:49 +0530

    Update products store

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
-<script>
+<script lang="ts">
+  import { afterUpdate } from "svelte";
   import "../app.css";
+  import type { LayoutData } from "./$types";
+  import { products } from "$lib/stores/product";
+
+  export let data: LayoutData;
+
+  afterUpdate(() => {
+    $products = data.products;
+    console.log($products);
+  });
 </script>
 
 <slot />
