06ee3695f4ac815902ab7fdc467a5fa505df9d38
santosh-sahu-7 <s.santoshsah0027@gmail.com>
2024-06-12 22:47:28 +0530

    Get categories

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,11 @@
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async ({ fetch }) => {
-    const response = await fetch('https://fakestoreapi.com/products?limit=6')
+    const response = await fetch('https://fakestoreapi.com/products')
     const result = await response.json()
-    // console.log(result)
-    return { products: result }
+
+    const categoriesResponse = await fetch('https://fakestoreapi.com/products/categories')
+    const categoriesResult = await categoriesResponse.json()
+    return { products: result, categories: categoriesResult }
 };
+
