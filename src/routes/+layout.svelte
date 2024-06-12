aa9c08906656490272332850c48b060395b8699f
santosh-sahu-7 <s.santoshsah0027@gmail.com>
2024-06-12 23:06:24 +0530

    Cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,13 @@
 <script lang="ts">
-  import { afterUpdate } from "svelte";
   import "../app.css";
   import type { LayoutData } from "./$types";
-  import { products } from "$lib/stores/product";
+  import { categories, products } from "$lib/stores/product";
   import Navbar from "$lib/components/Navbar.svelte";
 
   export let data: LayoutData;
 
-  afterUpdate(() => {
-    $products = data.products;
-    console.log($products);
-  });
+  $products = data.products;
+  $categories = data.categories;
 </script>
 
 <div class="max-w-7xl mx-auto min-h-screen px-5">
