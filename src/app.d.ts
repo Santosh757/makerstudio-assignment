f9310fe150ceee318b312577c11f48f56bb68007
santosh-sahu-7 <s.santoshsah0027@gmail.com>
2024-06-12 16:28:33 +0530

    Add Rating and Product type

--- src/app.d.ts
+++ src/app.d.ts
@@ -10,4 +10,17 @@ declare global {
 	}
 }
 
-export {};
+interface Rating {
+	rate: number
+	count: number
+}
+
+interface Product {
+	id: number
+	title: string
+	image: string
+	price: number
+	description: string
+	category: string
+	rating: Rating
+}
