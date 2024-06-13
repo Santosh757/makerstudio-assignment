28643eb430302528734ae892f5d541ed093710f7
santosh-sahu-7 <s.santoshsah0027@gmail.com>
2024-06-13 18:24:54 +0530

    Add cartItems store

--- src/lib/stores/product.ts
+++ src/lib/stores/product.ts
@@ -1,4 +1,6 @@
 import { writable } from "svelte/store";
+import { persisted } from 'svelte-persisted-store'
 
 export const products = writable<Product[]>([]);
 export const categories = writable<Category[]>([]);
+export const cartItems = persisted('cartItems', [])
