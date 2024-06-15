5cba3fe22a4902c246f08a320f63955ea345c81b
santosh-sahu-7 <s.santoshsah0027@gmail.com>
2024-06-15 11:30:24 +0530

    Add items count in the cart and style chnages

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,6 +1,20 @@
+<script lang="ts">
+  import { cartItems } from "$lib/stores/product";
+
+  let itemsCount: number;
+  $: {
+    itemsCount = $cartItems.reduce(
+      (total: number, item: CartItem) => total + item.quantity,
+      0
+    );
+  }
+</script>
+
 <header class="flex items-center justify-between py-3">
-  <img class="h-12 object-contain" src="/logo.png" alt="" />
-  <button>
+  <a href="/">
+    <img class="h-12 object-contain" src="/logo.png" alt="" />
+  </a>
+  <a href="/cart" class="relative flex items-end space-x-2">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
@@ -9,10 +23,19 @@
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      class="lucide lucide-shopping-cart h-6 w-6 text-gray-500">
+      class="lucide lucide-shopping-cart h-8 w-8 text-gray-500">
       <circle cx="8" cy="21" r="1" /><circle cx="19" cy="21" r="1" />
       <path
         d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12" />
     </svg>
-  </button>
+    <span class="font-semibold">Cart</span>
+    {#if itemsCount > 0}
+      <div
+        class="absolute -top-2 left-4 h-5 w-5 rounded-full bg-yellow-400 grid place-items-center">
+        <p class="text-xs font-medium">
+          {itemsCount > 9 ? "9+" : itemsCount}
+        </p>
+      </div>
+    {/if}
+  </a>
 </header>
