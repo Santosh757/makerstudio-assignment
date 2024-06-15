d48bba28306ed2c9eb18ce4b1049001ca41e0b7b
santosh-sahu-7 <s.santoshsah0027@gmail.com>
2024-06-15 19:18:36 +0530

    Create changeQuantity function

--- src/lib/stores/product.ts
+++ src/lib/stores/product.ts
@@ -3,4 +3,14 @@ import { persisted } from 'svelte-persisted-store'
 
 export const products = writable<Product[]>([]);
 export const categories = writable<Category[]>([]);
-export const cartItems = persisted<CartItem[]>('cartItems', [])
+export const cartItems = persisted<CartItem[]>('cartItems', []);
+
+export const changeQuantity = (id: number, increment: boolean = true) => {
+    cartItems.update((items: CartItem[]) =>
+        items.map((item: CartItem) =>
+            item.id === id
+                ? { ...item, quantity: item.quantity + (increment ? 1 : -1) }
+                : item
+        )
+    );
+};
