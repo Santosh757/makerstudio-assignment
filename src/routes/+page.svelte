45c3194fdd33e37c9fb7e85050429b3f4846fa58
santosh-sahu-7 <s.santoshsah0027@gmail.com>
2024-06-16 00:01:14 +0530

    Corrections related to pagenation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,13 @@
 <script lang="ts">
   import FilterAndSort from "$lib/components/FilterAndSort.svelte";
-  import { products } from "$lib/stores/product";
-
-  let currentPage: number = 0;
-  let totalPages: Product[][] = [];
-  let itemsPerPage: number = 6;
-
-  $: currentPageRows = totalPages[currentPage] ?? [];
-
-  const paginate = (items: Product[]) => {
-    const pages = Math.ceil(items.length / itemsPerPage);
-
-    totalPages = Array.from({ length: pages }, (_, index) => {
-      const start = index * itemsPerPage;
-      return items.slice(start, start + itemsPerPage);
-    });
-    currentPageRows = totalPages[currentPage];
-  };
-
-  const setPage = (p: number) => {
-    if (p >= 0 && p < totalPages.length) {
-      currentPage = p;
-    }
-  };
+  import {
+    currentPage,
+    currentPageRows,
+    paginate,
+    products,
+    setPage,
+    totalPages,
+  } from "$lib/stores/product";
 
   paginate($products);
 </script>
@@ -30,7 +15,7 @@
 <main class="mt-10 pb-10">
   <FilterAndSort />
   <div class="grid grid-cols-3 gap-8 mt-8">
-    {#each currentPageRows as product}
+    {#each $currentPageRows as product}
       <a
         href={`/${product.id}`}
         class="p-4 rounded-lg shadow-md w-full overflow-hidden border-[0.5px]">
@@ -45,18 +30,18 @@
   </div>
   <div class="mt-6 space-x-2">
     <button
-      on:click={() => setPage(currentPage - 1)}
+      on:click={() => setPage($currentPage - 1)}
       class="px-2 py-0.5 border rounded-md">
       Prev
     </button>
-    {#each totalPages as _, i}
+    {#each $totalPages as _, i}
       <button
         on:click={() => setPage(i)}
-        class="px-2 py-0.5 border rounded-md {currentPage == i &&
+        class="px-2 py-0.5 border rounded-md {$currentPage == i &&
           'bg-blue-500 text-white'}">{i + 1}</button>
     {/each}
     <button
-      on:click={() => setPage(currentPage + 1)}
+      on:click={() => setPage($currentPage + 1)}
       class="px-2 py-0.5 border rounded-md">
       Next
     </button>
